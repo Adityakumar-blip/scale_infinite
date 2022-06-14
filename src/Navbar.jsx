9e2082f4d2303af2a039fc09876ce91719163f76
adityakumar-blip <adityakamal303@gmail.com>
2022-06-14 12:13:04 +0530

    changes 1.1

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Scale Infinte</a>
+  <a className="navbar-brand" href="#">Assignment</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
